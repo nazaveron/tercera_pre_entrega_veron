5e74444b92b3333c112783bbd72b0ae05da9ab0f
naza <jnazarenaveron@gmail.com>
2023-09-28 14:34:23 -0300

    Esta es mi tercera pre-entrega VERON

---
+++ cinema/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class movie(models.Model):
+    titulo = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    duracion = models.PositiveIntegerField()
+    def __str__(self):
+        return self.titulo
+class usuario(models.Model):
+    email = models.EmailField(primary_key=True)
+    edad = models.PositiveIntegerField()
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    def __str__(self):
+        return self.apellido, self.email
+    
+class funcion(models.Model):
+    pelicula = models.ForeignKey(movie, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    sala = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    entradas_disponibles = models.PositiveIntegerField()
---
+++ cinema/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from cinema.views import *
+
+urlpatterns = [
+    path('peliculas/', peliculas, name="Movie"),
+    path('login/', user, name="User"),
+    path('funciones/', funciones, name="Funciones"),
+    path('registrarse/', Registrarse, name="Registro_Usuario"),
+    path('buscarPelicula/', buscar_pelicula, name="buscar_pelicula"),
+    path('resultados/', resultados, name="resultados")
+    ]
